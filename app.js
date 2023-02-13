0fee64b51bd744a621d425ff71067a808e35c09c
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2023-02-13 08:31:54 -0300

    update bootstramp

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 
+const path = require('path')
+
 const admin = require('./routes/admin')
 
 //const mongoose = require('mongoose')
@@ -20,6 +22,8 @@ app.set('view engine', 'handlebars')
 //mongoose
 
 
+//public
+app.use(express.static(path.join(__dirname,"public")))//a pasta que guarda nossos arquivos estáticos é a pasta public
 
 //rotas
 app.use('/admin', admin)
